43b42d06e3221a5868095d4066e840d0288e383f
rainvou <rainvou@groundxui-Macmini.local>
2025-02-25 00:23:49 +0900

    오늘은 list, top -o cpu, kill -9

---
+++ me/20250224/my_list.py
@@ -0,0 +1,48 @@
+squares = [1, 3, 9, 16, 25]
+
+print("====================================")
+print("                  ")
+
+
+# print(squares)
+# print(squares[0])
+# print(squares[2])
+# print(squares[-3])
+
+# print(squares[1:3])
+
+
+# print(squares[2:])
+# print("-----------------")
+
+
+
+# new_list = squares + [3]
+# print(new_list)
+# print("-----------------")
+
+
+# [37, 53, 73, 97, 125]
+
+# word = "jiwanwww"
+# cube = [1, 4, 9, 16, 25]
+# print(cube)
+# cube[2] = 99
+# print(cube + [1, 2, 3, 4, 5])
+
+# word.count("w")
+
+# print(word.count("w"))
+
+# cube.append(10000)
+# print(cube)
+
+
+cube = [1, 4, 9, 16, 25]
+
+for i in range(10000000000000000):
+    print(i, '번째 실행중')
+    cube.append(i + 1)
+
+print('끝!!!')
+# print(cube)
